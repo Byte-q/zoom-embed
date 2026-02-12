f0bb58e6e104dbfe3755d7de4d9c466e9ce2689a
Mohammed abdalaziz <mohaazizz01@gmail.com>
2026-02-12 10:11:32 +0200

    edit csp settings

--- next.config.mjs
+++ next.config.mjs
@@ -24,7 +24,8 @@ const nextConfig = {
             key: "Content-Security-Policy",
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.zoom.us https://zoom.us https://zoom.com https://vercel.live https://*.stream-io-video.com;
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://zoom.us https://*.zoom.us https://zoom.com https://*.zoom.com https://source.zoom.us https://vercel.live https://*.stream-io-video.com;
+              script-src-elem 'self' 'unsafe-inline' https://zoom.us https://*.zoom.us https://zoom.com https://*.zoom.com https://source.zoom.us;
               frame-src 'self' https://*.zoom.us https://zoom.us https://zoom.com https://*.zoom.com https://*.vercel.live https://vercel.live/ https://*.stream-io-video.com https://zoom-embed.vercel.app/;
               connect-src 'self' https://*.zoom.us wss://*.zoom.us https://*.stream-io-video.com https://zoom.us https://zoom.com https://vercel.live https://*.vercel.live;
               img-src 'self' data: blob: https://*.zoom.us https://zoom.us https://zoom.com https://*.vercel.live https://vercel.live/ https://*.stream-io-video.com;
