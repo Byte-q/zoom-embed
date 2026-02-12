a8e02b038f1a7fe48eefe1571a4557b2813b01dd
Mohammed abdalaziz <mohaazizz01@gmail.com>
2026-02-12 10:19:09 +0200

    edit csp settings 5

--- next.config.mjs
+++ next.config.mjs
@@ -26,8 +26,9 @@ const nextConfig = {
               default-src 'self';
               script-src 'self' 'unsafe-inline' 'unsafe-eval' https://zoom.us https://*.zoom.us https://zoom.com https://*.zoom.com https://source.zoom.us https://vercel.live https://*.stream-io-video.com;
               script-src-elem 'self' 'unsafe-inline' https://zoom.us https://*.zoom.us https://zoom.com https://*.zoom.com https://source.zoom.us;
+              worker-src 'self' blob:;
               frame-src 'self' https://*.zoom.us https://zoom.us https://zoom.com https://*.zoom.com https://*.vercel.live https://vercel.live/ https://*.stream-io-video.com https://zoom-embed.vercel.app/;
-              connect-src 'self' https://*.zoom.us wss://*.zoom.us https://*.stream-io-video.com https://zoom.us https://zoom.com https://vercel.live https://*.vercel.live;
+              connect-src 'self' https://*.zoom.us wss://*.zoom.us https://*.stream-io-video.com https://zoom.us https://zoom.com https://vercel.live https://*.vercel.live https://source.zoom.us https://dmogdx0jrul3u.cloudfront.net https://d1cdksi819e9z7.cloudfront.net;
               img-src 'self' data: blob: https://*.zoom.us https://zoom.us https://zoom.com https://*.vercel.live https://vercel.live/ https://*.stream-io-video.com;
               style-src 'self' 'unsafe-inline';
             `.replace(/\n/g, ""),
