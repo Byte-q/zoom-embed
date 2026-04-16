16f0e425d6f63e6bc733bfd3e24167bf5a306605
Mohammed Abdalaziz <mohaazizz01@gmail.com>
2026-04-16 09:00:10 +0200

    Update connect-src to include Supabase domain

--- next.config.mjs
+++ next.config.mjs
@@ -28,7 +28,7 @@ const nextConfig = {
               script-src-elem 'self' 'unsafe-inline' https://zoom.us https://*.zoom.us https://zoom.com https://*.zoom.com https://source.zoom.us;
               worker-src 'self' blob:;
               frame-src 'self' https://*.zoom.us https://zoom.us https://zoom.com https://*.zoom.com https://*.vercel.live https://vercel.live/ https://*.stream-io-video.com https://zoom-embed.vercel.app/;
-              connect-src 'self' https://*.zoom.us wss://*.zoom.us https://*.stream-io-video.com https://zoom.us https://zoom.com https://vercel.live https://*.vercel.live https://source.zoom.us https://dmogdx0jrul3u.cloudfront.net https://d1cdksi819e9z7.cloudfront.net;
+              connect-src 'self' https://*.supabase.co https://*.zoom.us wss://*.zoom.us https://*.stream-io-video.com https://zoom.us https://zoom.com https://vercel.live https://*.vercel.live https://source.zoom.us https://dmogdx0jrul3u.cloudfront.net https://d1cdksi819e9z7.cloudfront.net;
               img-src 'self' data: blob: https://*.zoom.us https://zoom.us https://zoom.com https://*.vercel.live https://vercel.live/ https://*.stream-io-video.com;
               style-src 'self' 'unsafe-inline' https://source.zoom.us;
             `.replace(/\n/g, ""),
