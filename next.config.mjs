add03d56408b385078c6b8ef5c547f8e5c7a7b25
Mohammed Abdalaziz <mohaazizz01@gmail.com>
2026-04-16 09:29:28 +0200

    Update Content-Security-Policy for script and style sources

--- next.config.mjs
+++ next.config.mjs
@@ -25,12 +25,12 @@ const nextConfig = {
             value: `
               default-src 'self';
               script-src 'self' 'unsafe-inline' 'unsafe-eval' https://zoom.us https://*.zoom.us https://zoom.com https://*.zoom.com https://source.zoom.us https://vercel.live https://*.stream-io-video.com;
-              script-src-elem 'self' 'unsafe-inline' https://zoom.us https://*.zoom.us https://zoom.com https://*.zoom.com https://source.zoom.us;
+              script-src-elem 'self' 'unsafe-inline' blob: https://zoom.us https://*.zoom.us https://zoom.com https://*.zoom.com https://source.zoom.us;
               worker-src 'self' blob:;
               frame-src 'self' https://*.zoom.us https://zoom.us https://zoom.com https://*.zoom.com https://*.vercel.live https://vercel.live/ https://*.stream-io-video.com https://zoom-embed.vercel.app/;
               connect-src 'self' https://*.supabase.co https://*.zoom.us wss://*.zoom.us https://*.stream-io-video.com https://zoom.us https://zoom.com https://vercel.live https://*.vercel.live https://source.zoom.us https://dmogdx0jrul3u.cloudfront.net https://d1cdksi819e9z7.cloudfront.net;
               img-src 'self' data: blob: https://*.zoom.us https://zoom.us https://zoom.com https://*.vercel.live https://vercel.live/ https://*.stream-io-video.com;
-              style-src 'self' 'unsafe-inline' https://source.zoom.us;
+              style-src 'self' 'unsafe-inline' https://source.zoom.us https://*.zoom.us;
             `.replace(/\n/g, ""),
           },
         ],
